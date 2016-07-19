6a0351a1f246718a7154488402928b5f1530d2a2
BLHaas <bethhaas@Laetitia.local>
2016-07-19 11:13:51 -0400

    msdCalculator works as a function
    Added:
    - MSD calculated from displacement data
    - Input data imported
    - Output data saved as .csv and .mat
    - MSD curve plotted & saved
    - Progress text prints to Command Window
    Changed
    - Now works as a function
    - Import data fixed (comma-delimited input, not tab-delimited)

--- msdCalculator.m
+++ msdCalculator.m
@@ -1,4 +1,4 @@
-% function msdCalculator
+function msdCalculator
 %% About msdCalculator
 % Works with 1 track only. Reads data from csv file with 2 columns 
 % (X and Y positions). Calculates mean squared displacement over
@@ -13,11 +13,28 @@ xColumn = 1;
 yColumn = 2;
 
 %% Import data file
-% To do: inputPath = 
-molPositions = importdata(inputPath, '\t', 1);
+disp ('Choose a data file.')
+[inputFile, inputPath] = uigetfile('*.csv', ...
+    'Choose a data file');
+if isequal(inputFile, 0) || isequal(inputPath, 0)
+    disp('User canceled.')
+    return
+end
+molPositions = importdata(fullfile(inputPath, inputFile), ',', 1);
 
 %% Prepare output file
-% To do
+disp('Choose an output folder.')
+outputFile = [datestr(now, 'yyyy-mm-dd HHMMSS') ' MSD'];
+outputPath = uigetdir('', 'Choose an output folder');
+if isequal(outputPath, 0)
+    disp('User canceled.')
+    return
+end
+outputFID = fopen([fullfile(outputPath, outputFile) '.csv'], 'w');
+if outputFID == -1
+    disp('Error. Cannot open output file.')
+    return
+end
 
 %% Determine maximum frame difference to use
 nFrames = size(molPositions.data, 1); % #rows = #frames
@@ -27,6 +44,7 @@ nFrameDiff = floor(fracFrameDiffs*nFrames);
 displacementData = NaN(nFrames - 1, nFrameDiff);
 
 %% Calculate displacements
+disp('Calculating displacements')
 for iFrameDiff = 1:nFrameDiff % choose a tau value (frame difference)
     nRow = nFrames - iFrameDiff;
     for iRow = 1:nRow % calculate displacements at the chosen tau value
@@ -38,10 +56,36 @@ for iFrameDiff = 1:nFrameDiff % choose a tau value (frame difference)
     end
 end
 
-%% To be continued
-% mean(displacementData.^2, 'omitnan')'
-% store MSD values with their respective tau values
-% plot MSD vs. tau
-% save data & plot
+%% Store MSD and tau values
+msdData(:, 1) = (1:nFrameDiff)'; % tau values
+msdData(:, 2) = mean(displacementData.^2, 'omitnan')'; % MSD values
 
-% end of function
+%% Plot MSD curve
+% Create figure window
+msdFigure = figure;
+msdFigure.Color = 'w'; % white figure background instead of gray
+% Plot
+plot(msdData(:,1), msdData(:,2));
+% Label axes
+msdAxes = gca;
+msdAxes.XLabel.String = 'frame difference';
+msdAxes.YLabel.String = 'mean squared displacement';
+msdAxes.Title.String = ['MSD of ' inputFile(1:length(inputFile)-4)];
+
+%% Save data & figure
+disp('Data saved as *.csv, *.mat, and *.fig')
+% Write MSD data to *.csv file
+fprintf(outputFID, ['%d' ',' '%.4f' ',' '\n'], msdData');
+fprintf(outputFID, ['Input data from ' fullfile(inputPath, inputFile)]);
+% Save MSD and displacement data to *.mat file
+save(fullfile(outputPath, outputFile), 'msdData', 'displacementData');
+% Save MSD figure to *.fig file
+savefig(msdFigure, fullfile(outputPath, outputFile))
+
+% Close *.csv file
+closeFlag = fclose(outputFID);
+if closeFlag == -1
+    disp('Error. Could not close *.csv file.')
+    return
+end
+end
